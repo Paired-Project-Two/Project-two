d59e1624532712770baa27a3e2be67de5cc98bd0
Umer <umeraziz4545@gmail.com>
2022-01-10 11:10:43 -0700

    working on nav - hamburger menu RSD, base stylings done. Not 100% on colour

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const jobApp = {};
 
-jobApp.init = function()  {
+jobApp.init = function () {
   //jobApp.grabData();
   jobApp.captureUserInput();
-}
-const submitButton = document.querySelector(".searchButton")
-const category = document.querySelector(".jobCategory")
-const level = document.querySelector(".userLevel")
-const resultsDiv = document.querySelector('.results');
+};
+const submitButton = document.querySelector(".searchButton");
+const category = document.querySelector(".jobCategory");
+const level = document.querySelector(".userLevel");
+const resultsDiv = document.querySelector(".results");
 
 jobApp.captureUserInput = () => {
   submitButton.addEventListener("click", () => {
@@ -15,17 +15,16 @@ jobApp.captureUserInput = () => {
     const categoryChoice = category.value;
     console.log(categoryChoice);
     const cityChoice = city.value;
-    console.log(cityChoice)
+    console.log(cityChoice);
     const levelChoice = level.value;
     console.log(levelChoice);
-    
+
     jobApp.insertUserInput(categoryChoice, levelChoice, cityChoice);
-  })
-}
+  });
+};
 
 jobApp.insertUserInput = (category, level, city) => {
-  jobApp.proxiedUrl =
-    `https://www.themuse.com/api/public/jobs?page=1&category=${category}&location=${city}&level=${level}&descending=`;
+  jobApp.proxiedUrl = `https://www.themuse.com/api/public/jobs?page=1&category=${category}&location=${city}&level=${level}&descending=`;
 
   const url = new URL("http://proxy.hackeryou.com");
   url.search = new URLSearchParams({
@@ -35,60 +34,48 @@ jobApp.insertUserInput = (category, level, city) => {
     // "params[page]": 1,
     // "params[descending]": true,
     "proxyHeaders[header]": "GET",
-  
-  })
+  });
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.results);
-       jobApp.filterResults(data.results);
+      jobApp.filterResults(data.results);
     });
-}
-
+};
 
 jobApp.filterResults = (results) => {
-  
   results.filter((category) => {
     // console.log(category.levels[0].name);
-<<<<<<< HEAD
     const jobLocation = category.locations.forEach((jobLocation) => {
-      console.log(jobLocation)
-    })
-    console.log(jobLocation)
-    const results = document.querySelector('.results');
-=======
-    
-    
-   
-   
->>>>>>> c476f0d8a65c8e72099774b9636eb2ab3f7b07fe
-    
-    const listings = document.querySelector('.listings');
+      console.log(jobLocation);
+    });
+    console.log(jobLocation);
+    const results = document.querySelector(".results");
 
-    const listing = document.createElement('li');
+    const listings = document.querySelector(".listings");
 
-    listing.innerHTML = 
-    `
+    const listing = document.createElement("li");
+
+    listing.innerHTML = `
     <h3 class='jobTitle'> ${category.name} </h3>
 
     <p class='jobDescription'> ${category.contents} </p>
     <p class='jobURL'>Job Portal: <a href="${category.refs.landing_page}">Portal Link</a></p>
     <p class='jobLocation'>Primary Location: </p>
 
-    `
+    `;
 
-    listings.appendChild(listing)
+    listings.appendChild(listing);
     category.locations.forEach((jobLocation) => {
-      listing.innerHTML += `<p>${jobLocation.name}.  </p>`
-    })
+      listing.innerHTML += `<p>${jobLocation.name}.  </p>`;
+    });
+  });
 
-  }) 
-  
   // jobApp.displayResults(listings);
-}
+};
 
 // jobApp.displayResults = (listingsObjects) => {
-  // console.log(listingsObjects)
+// console.log(listingsObjects)
 
 // }
 
